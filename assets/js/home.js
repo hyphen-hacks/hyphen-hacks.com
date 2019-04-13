de09fa20a74057c63663456832b1af77ae1ea5f1
190n <benjamin.j.grant@gmail.com>
2019-04-13 11:56:12 -0700

    Rewrite scroll animations JS

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,34 +1,62 @@
-const breakpointElem = document.getElementById('scroll-breakpoint'),
-    navElem = document.getElementById('navbar');
+const anims = [
+    {
+        trigger: 'scroll-breakpoint',
+        target: 'navbar',
+        class: 'navbar-hidden',
+        once: false
+    }
+];
 
-let breakpointOffset = breakpointElem.offsetTop;
-
-function update(isIntersectionObserver, entries) {
-    if (isIntersectionObserver === true) {
+function trigger(isIntersectionObserver, anim, entries, observer) {
+    if (isIntersectionObserver) {
         if (entries[0].isIntersecting) {
-            navElem.classList.add('navbar-hidden');
+            anim.$target.classList.add(anim.class);
+            if (anim.once) {
+                observer.disconnect();
+                anim.ignore = true;
+            }
         } else {
-            navElem.classList.remove('navbar-hidden');
+            anim.$target.classList.remove(anim.class);
         }
     } else {
-        if (window.pageYOffset > breakpointOffset) {
-            navElem.classList.remove('navbar-hidden');
-            return;
-        } else {
-            navElem.classList.add('navbar-hidden');
+        // do all animations
+        for (let i in anims) {
+            let a = anims[i];
+            if (a.ignore) {
+                continue;
+            }
+
+            if (window.pageYOffset <= a.triggerOffset && !a.$target.classList.contains(a.class)) {
+                a.$target.classList.add(a.class);
+                if (a.once) {
+                    a.ignore = true;
+                }
+            } else if (window.pageYOffset > a.triggerOffset && a.$target.classList.contains(a.class)) {
+                a.$target.classList.remove(a.class);
+            }
         }
     }
 }
 
 window.addEventListener('resize', () => {
-    breakpointOffset = breakpointElem.offsetTop;
+    for (let a of anims) {
+        a.triggerOffset = a.$trigger.offsetTop;
+    }
 });
 
-if (typeof window.IntersectionObserver == 'function') {
-    let observer = new IntersectionObserver(update.bind(null, true));
-    observer.observe(breakpointElem);
-} else {
-    window.addEventListener('scroll', update.bind(null, false), false);
+for (let a of anims) {
+    a.$trigger = document.getElementById(a.trigger);
+    a.$target = document.getElementById(a.target);
+    a.triggerOffset = a.$trigger.offsetTop;
 }
 
-update(false);
+if (typeof window.IntersectionObserver == 'function') {
+    for (let a of anims) {
+        let observer = new IntersectionObserver(trigger.bind(null, true, a));
+        observer.observe(a.$trigger);
+    }
+} else {
+    window.addEventListener('scroll', trigger.bind(null, false), false);
+}
+
+trigger(false);
