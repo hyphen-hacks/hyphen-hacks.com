5b6e8472b278769ab6bab10ffcbef7e1a3ab83fc
190n <benjamin.j.grant@gmail.com>
2019-04-24 10:24:57 -0700

    Use JavaScript for mailing list signup

---
+++ assets/js/mailing-list-signup.js
@@ -0,0 +1,37 @@
+document.getElementById('ml-signup-form').addEventListener('submit', submitSignup, false);
+
+
+function submitSignup(e) {
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+
+    let status = document.getElementById('ml-signup-status'),
+        email = document.getElementById('ml-email').value,
+        submit = document.getElementById('ml-submit');
+
+    submit.disabled = true;
+    status.innerHTML = 'Submitting...';
+
+    let xhr = new XMLHttpRequest;
+    xhr.open('POST', e.target.action, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    function logError() {
+        status.innerHTML = 'An error occurred. Please try again later.';
+        status.classList.add('has-text-danger');
+    }
+
+    xhr.onerror = logError;
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            status.innerHTML = 'You\'ve successfully been added to our mailing list. Thanks!';
+        } else {
+            logError();
+        }
+    };
+
+    xhr.send('email=' + encodeURIComponent(email));
+
+    return false;
+}
