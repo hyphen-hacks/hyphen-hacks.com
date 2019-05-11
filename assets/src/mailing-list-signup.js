7b26864827278bc1e0e566c331f34ab48a3d7b60
190n <benjamin.j.grant@gmail.com>
2019-05-10 21:31:09 -0700

    Add loading icon to mailing list signup form

--- assets/src/mailing-list-signup.js
+++ assets/src/mailing-list-signup.js
@@ -12,6 +12,7 @@ function submitSignup(e) {
         email = document.getElementById('ml-email').value,
         emailError = document.getElementById('email-error-text'),
         submit = document.getElementById('ml-submit'),
+        spinner = document.getElementById('ml-submit-spinner'),
         emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     if (!consent.checked) {
@@ -30,14 +31,16 @@ function submitSignup(e) {
 
     submit.disabled = true;
     status.innerHTML = 'Submitting...';
+    spinner.style.display = 'inline-flex';
 
     let xhr = new XMLHttpRequest;
     xhr.open('POST', e.target.action, true);
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
     function logError() {
+        spinner.style.display = 'none';
         status.innerHTML = 'An error occurred. Please try again later.';
-        status.classList.add('is-danger');
+        status.classList.add('has-text-danger');
         if (typeof _paq == 'object') {
             _paq.push(['trackEvent', 'Marketing', 'ListSignupError']);
         }
@@ -46,7 +49,8 @@ function submitSignup(e) {
     xhr.onerror = logError;
     xhr.onload = () => {
         if (xhr.status == 200) {
-            status.classList.add('is-success');
+            spinner.style.display = 'none';
+            status.classList.add('has-text-success');
             status.innerHTML = 'You\'ve successfully been added to our mailing list. Thanks!';
             if (typeof _paq == 'object') {
                 _paq.push(['trackEvent', 'Marketing', 'ListSignupSuccess']);
