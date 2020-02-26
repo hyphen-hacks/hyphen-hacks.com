beab5db63004dbf6beb0435206524c9edcd8c92e
Ronan <ronan@ronanfuruta.com>
2020-02-26 10:18:47 -0800

    cleaning out

--- dev-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-concurrently -k -n "webpack,hugo" -c "yellow.bold,cyan.bold" "(cd assets; npx webpack --watch --progress)" "hugo server --appendPort=false -b / --bind 0.0.0.0 -D --disableFastRender"
