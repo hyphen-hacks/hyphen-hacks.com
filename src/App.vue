6c5091cf5b46d28ebcb86f5332342a7e8369a678
Ronan <ronan@ronanfuruta.com>
2020-03-10 22:27:36 -0700

    navbard

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="flex flex-row items-center bg-gray-lighter p-5">
+      <div class="flex flex-row items-center mr-auto">
+        <img class="h-10 mr-1" src="./assets/logo.svg" alt="Hyphen-Hacks logo">
+        <h1 class="font-bold text-2xl">Hyphen-Hacks 2020</h1>
+      </div>
+
+      <a class="nav_link" href="">MISSION</a>
+      <a class="nav_link"  href="">CONTACT</a>
+      <a class="nav_link"  href="">FAQ</a>
+      <a class="nav_link btn--outline" href="">Volunteer</a>
+      <a class="nav_link btn--outline" href="">Mentor</a>
+      <a class="nav_link btn" href="">Apply</a>
+
+    </nav>
     <router-view/>
   </div>
 </template>
--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,23 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          default: "#E09215",
+          darker: "#85560e"
+        },
+        gray: {
+          default: "#191919",
+          lighter: "#333333",
+          hover: "#c9c9c9",
+          lightest: "#EFEFEF"
+        },
+
+      },
+    },
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus']
+  },
   plugins: [],
 }
