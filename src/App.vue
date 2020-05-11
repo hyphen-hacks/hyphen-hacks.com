e12539035c6e3e8370a5c0e685e9d78e2c8f05b9
Ronan <ronan@ronanfuruta.com>
2020-05-10 21:15:03 -0700

    added COVID banner

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+  <div class="banner">
+    <p> We are monitoring the COVID-19 outbreak and considering several options to provide the best Hyphen-Hacks experience possible. Keep checking here and sign up for our mailing list to stay informed about our plans.</p>
+  </div>
     <router-view/>
   </div>
 </template>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="layout">
+    <div class="banner">
+      <p>We are monitoring the COVID-19 outbreak and considering several options to provide the best Hyphen-Hacks experience possible. Keep checking here to stay informed about our plans.</p>
+    </div>
     <slot/>
   </div>
 </template>
